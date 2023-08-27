99ebf62600104fa2bf3e979fe288c96a2e1e1e7c
Berkay Çongal <berkaycongal@hotmail.com>
2023-08-28 01:03:35 +0300

    ödev bitti

--- js/custom.js
+++ js/custom.js
@@ -1,6 +1,7 @@
 const postsContainer = document.querySelector("#posts-container");
 const postTitle = document.querySelector("#post-titles");
 const postSlug = document.querySelector("#post-slug");
+const commentForm = document.querySelector('.comment form');
 const baseurl = "http://localhost:1337/api/"
 
 
@@ -8,6 +9,7 @@ async function getPosts() {
 let posts = await fetch("http://localhost:1337/api/posts?populate=*")
   .then(response => response.json())
   loadPage(posts.data)
+  
 }
 
 function loadPage(posts)  {
@@ -16,10 +18,12 @@ function loadPage(posts)  {
       <a href="#/posts/${post.id}">
                <h2 class="post-title">${post.attributes.title}</h2>
                 <h3 class="post-subtitle">${post.attributes.content}</h3>
+                <h4 class=""post-sunmay>${post.attributes.sunmmary}</h4>
            </a>
             <p class="post-meta">
                  Posted by
                  <a href="#!">${post.attributes.user.data.attributes.username}</a>
+                 
                  on ${post.attributes.updatedAt.substr(0,10)}
              </p>
         </div>
@@ -27,6 +31,7 @@ function loadPage(posts)  {
    });
 
    window.addEventListener("hashchange", () => {
+    createComments()
     let  url = location.hash.substr(2);
     if(url === "") {
         loadPage(posts)
@@ -43,33 +48,63 @@ function loadPage(posts)  {
                 </div>
             </div>
         </div>
+        <hr>
         `
+        createComments();
+        commentForm.classList.toggle("active")
+        console.log(result.data.attributes.content);
         postTitle.textContent = result.data.attributes.title
         postSlug.textContent = result.data.attributes.slug
+
         }) 
     }
     
-   } )
+   })
    
 }
 
 
 
+const createComments = async () => {
+    let comments = await fetch('http://localhost:1337/api/comments').then(comment => comment.json())
+    comments.data.forEach( element => {
+       postsContainer.innerHTML += `
+            <div class="comment-list">
+                <h1>${element.attributes.name}</h1>
+                <p>${element.attributes.comment}</p>
+            </div>
+       `
+    });
+} 
 
+
+commentForm.addEventListener('submit', function (e) {
+
+    e.preventDefault();
+    const formData = new FormData(commentForm);
+    const formObj = Object.fromEntries(formData);
+    formObj.post = 1;
+
+
+    fetch('http://localhost:1337/api/comments', {
+        method: 'POST',
+        body: JSON.stringify({ data: formObj }),
+        headers: {
+            "Content-Type": "application/json"
+        }
+
+    }).catch(function () {
+        alert('Gönderilemedi');
+    }).then(function (response) {
+        return response.json();
+    }).then(function (responseData) {
+        // Yorumları yeniden yükle
+        const postId = responseData.data.attributes.post;
+        // getComments(postId);
+        commentForm.reset();
+        createComments()
+    })
+    
+});
 
 getPosts()
-
-
-
-    // <div class="post-preview">
-    //         <a href="post.html">
-    //             <h2 class="post-title">${data.attributes.title}</h2>
-    //             <h3 class="post-subtitle">${data.attributes.content}</h3>
-    //         </a>
-    //         <p class="post-meta">
-    //             Posted by
-    //             <a href="#!">${data.attributes}</a>
-    //             on September 24, 2023
-    //         </p>
-    //     </div>
-    //     <hr class="my-4" />
